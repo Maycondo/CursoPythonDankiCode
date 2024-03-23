e814b185969f94ae519499072873fa574bdc579f
maycon doglas <maycondouglasniculau@gmail.com>
2024-03-23 19:05:25 -0300

    CursoPythonDankiCode

--- Strings.py
+++ Strings.py
@@ -10,4 +10,18 @@ print(w)
 a = "Curso de python"
 
 
+print(a)
+
 # Metedos de python
+
+b = " Objeto "
+
+print(b.split())
+
+c = "OLA MUNDO"
+
+print(c.lower())
+
+d = "HELLO MY NAME"
+
+print(d.upper())
